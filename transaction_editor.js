761e7fbbf05be66b728859c1942d3a1f0f9b94d6
Garret <jetaimed@gmail.com>
2023-08-02 15:13:42 -0400

    Added support for bulk adding transactions to database (speeding up process a lot) and also added support for saving progress on transaction emails editing.

--- transaction_editor.js
+++ transaction_editor.js
@@ -1,7 +1,8 @@
 const TransactionEditor = {
   props: ['transaction_data', 'accounts', 'categories'],
-  //emits: [''],
-  setup (props) {
+  emits: ['onModify'],
+  computed: { console: () => console },
+  setup (props, ctx) {
 	  const accounts = toRef(props, 'accounts')
 	  const filteredAccounts = ref([]);
 	  filteredAccounts.value = accounts.value
@@ -9,6 +10,8 @@ const TransactionEditor = {
 	  const categories = toRef(props, 'categories')
 	  const filteredCategories = ref([]);
 	  filteredCategories.value = categories.value
+	  let timeout = null;
+	  let tinytimeout = null;
 
 	  return {
 		  accounts,
@@ -35,12 +38,33 @@ const TransactionEditor = {
 				  const needle = val.toLowerCase()
 				  filteredCategories.value = categories.value.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
 			  })
+		  },
+		  delayedOnModify () {
+			  if (timeout !== null)
+			  {
+				  clearTimeout(timeout);
+			  }
+			  timeout = setTimeout( () => this.$emit('onModify'), 3000 );
+		  },
+		  tinyDelayedOnModify () {
+			  if (tinytimeout !== null)
+			  {
+				  clearTimeout(tinytimeout);
+			  }
+			  tinytimeout = setTimeout( () => { 
+				  if (timeout !== null)
+				  {
+					  clearTimeout(timeout);
+					  timeout = null;
+				  }
+				  this.$emit('onModify')
+			  }, 50 );
 		  }
 	  }
   },
   template: `
-    <q-select input-debounce="0" map-options use-input @filter="filterAccountsFn" outlined emit-value v-model="transaction_data.account" :options="filteredAccounts" label="Account"></q-select>
-    <q-input outlined v-model="transaction_data.date" mask="####-##-##">
+    <q-select input-debounce="0" map-options use-input @update:model-value="tinyDelayedOnModify();" @filter="filterAccountsFn" outlined emit-value v-model="transaction_data.account" :options="filteredAccounts" label="Account"></q-select>
+    <q-input outlined v-model="transaction_data.date" @update:model-value="tinyDelayedOnModify();" mask="####-##-##">
       <template v-slot:append>
         <q-icon name="event" class="cursor-pointer">
           <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -54,9 +78,10 @@ const TransactionEditor = {
       </template>
     </q-input>
 
-    <q-input v-model="transaction_data.description" label="Description/Payee" outlined></q-input>
-    <q-input v-model="transaction_data.amount" type="number" label="Amount($)" outlined></q-input>
-    <q-input v-model="transaction_data.comment" label="Comment" outlined></q-input>
-    <q-select input-debounce="0" map-options use-input @filter="filterCategoriesFn" outlined emit-value v-model="transaction_data.category" :options="filteredCategories" label="Category"></q-select>
+    <q-input v-model="transaction_data.description" @update:model-value="delayedOnModify()" @blur="tinyDelayedOnModify();" label="Description/Payee" outlined></q-input>
+			
+    <q-input v-model="transaction_data.amount" type="number" @update:model-value="delayedOnModify()" @blur="tinyDelayedOnModify();" label="Amount($)" outlined></q-input>
+    <q-input v-model="transaction_data.comment" @update:model-value="delayedOnModify()" @blur="tinyDelayedOnModify();" label="Comment" outlined></q-input>
+    <q-select input-debounce="0" map-options use-input @update:model-value="tinyDelayedOnModify();" @filter="filterCategoriesFn" outlined emit-value v-model="transaction_data.category" :options="filteredCategories" label="Category"></q-select>
   `
 }
